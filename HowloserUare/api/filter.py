e89ddb4f9d06a099400959466404b1bba36737bd
wotmshuaisi <charlanders1@gmail.com>
2018-03-09 10:36:16 +0800

    add filter option

--- HowloserUare/api/filter.py
+++ HowloserUare/api/filter.py
@@ -4,5 +4,7 @@ from vendor.request_params_filter.filter import BaseFilter
 class IncomeFilter(BaseFilter):
     fields = {
         'date': 'datetime__startswith',
-        'type': 'type'
+        'type': 'type',
+        'min_amount': 'amount__gte',
+        'max_amount': 'amount__lte'
     }
